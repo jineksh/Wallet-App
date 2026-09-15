7b3accd587cb48765a0053d6f59b1a158bbb76e7
jineksh <chovatiyajinex@gmail.com>
2026-09-15 10:55:31 +0530

    fix bug in types/txns

--- src/utils/txns.ts
+++ src/utils/txns.ts
@@ -1,13 +1,13 @@
-import { getPrismaClient} from '../config/db.js';
-import {ShardId} from '../types/shard.js'
+import { getPrismaClient } from '../config/db.js';
+import { ShardId } from '../types/shard.js';
 
 export async function executeInTransaction<T>(
     shardId: ShardId,
     fn: (tx: any) => Promise<T>
 ): Promise<T> {
-    const client = getPrismaClient(shardId);
+    const client = getPrismaClient(shardId) as any;
 
-    return client.$transaction(fn, {
+    return client.$transaction(async (tx: any) => await fn(tx), {
         isolationLevel: 'RepeatableRead',
     });
 }
